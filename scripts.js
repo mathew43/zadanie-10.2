d2716b2d42e740b145097870b3d56e7c1ee7c82b
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-16 21:09:49 +0100

    add start and pause slider

--- scripts.js
+++ scripts.js
@@ -1,7 +1,36 @@
-$(function(){
+$(function() {
 
-	setInterval(function(){
-		$('#slider .slides').animate({'margin-left': '-=400px'}, 1000;
-	}, 3000);
 
-})
+    var width = 400;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
